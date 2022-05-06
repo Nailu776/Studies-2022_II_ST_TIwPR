aa8c75ff8022d9ad8633ee21f76fe4af329651c0
Julian Helwig <julek.helwig@gmail.com>
2022-05-06 14:02:58 +0200

    PlayerSchemas cleaned.

--- ProjektRESTapi/REST_api/Schemas/PlayerSchemas.py
+++ ProjektRESTapi/REST_api/Schemas/PlayerSchemas.py
@@ -25,26 +25,6 @@ class PlayerSchema(object):
             format: int64
             default: 0
     """
-# @components.schemas.register
-# class PlayerPatchSchema(object):
-#     """
-#     ---
-#     type: object
-#     description: Player patch schema
-#     properties:
-#         points_record:
-#             type: integer
-#             format: int64
-#             default: 0
-#         no_msg_sended:
-#             type: integer
-#             format: int64
-#             default: 0
-#         no_msg_received:
-#             type: integer
-#             format: int64
-#             default: 0
-#     """
 @components.schemas.register
 class PlayerUpdateSchema(object):
     """
@@ -76,42 +56,3 @@ class PlayerPostSchema(object):
         nick:
             type: string
     """
-# @components.schemas.register
-# class ArrayOfPlayers(object):
-#     """
-#     ---
-#     definitions:
-#         ArrayOfPlayers:
-#             type: array
-#             items:
-#                 type: object
-#                 properties:
-#                     id:
-#                         type: integer
-#                         format: int64
-#                     nick:
-#                         type: string
-#                     points_record:
-#                         type: integer
-#                         format: int64
-#                         default: 0
-#                     no_msg_sended:
-#                         type: integer
-#                         format: int64
-#                         default: 0
-#                     no_msg_received:
-#                         type: integer
-#                         format: int64
-#                         default: 0
-#             example:
-#                 - id: 10
-#                   nick: TypicalNickName
-#                   points_record: 996
-#                   no_msg_sended: 125
-#                   no_msg_received: 200
-#                 - id: 11
-#                   nick: TypicalNickName2
-#                   points_record: 995
-#                   no_msg_sended: 124
-#                   no_msg_received: 201
-#     """
